c1ab2fc9f82a9cc35b6603d22b2ac5139f9cb686
DylanAustin-TheDreamer <austindylan0@gmail.com>
2025-08-14 12:26:17 +0100

    Give a breif outline of the functions I want in my JavaScript.

---
+++ assets/javascript/script.js
@@ -0,0 +1,23 @@
+function checkDifficulty(){
+    // Check the selected difficulty level
+}
+
+function startTest(){
+    // Start the typing test
+}
+
+function stopTest(){
+    // Stop the typing test
+}
+
+function calculateScore(){
+    // Calculate the user's typing speed and accuracy
+}
+
+function retryTest(){
+    // Retry the typing test
+}
+
+function certificate(){
+    // Generate a certificate for the user
+}
